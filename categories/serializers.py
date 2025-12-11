c90bb4d746d4381a7cbe704306d8a3e94e6ff8f9
Ajay122003 <ajayelango54@gmail.com>
2025-12-11 14:58:24 +0530

    update

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Category
+from products.models import Product
+from products.serializers import ProductSerializer
+
 
 class CategorySerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField(read_only=True)
+    products = serializers.SerializerMethodField(read_only=True)  # ✅ ADD THIS
 
     class Meta:
         model = Category
@@ -13,7 +17,8 @@ class CategorySerializer(serializers.ModelSerializer):
             "image",
             "image_url",
             "is_active",
-            "sort_order"
+            "sort_order",
+            "products",  # ✅ FRONTEND USES THIS
         ]
 
     def get_image_url(self, obj):
@@ -21,3 +26,14 @@ class CategorySerializer(serializers.ModelSerializer):
         if obj.image:
             return request.build_absolute_uri(obj.image.url)
         return None
+
+    def get_products(self, obj):
+        # Only active products inside this category
+        qs = Product.objects.filter(category=obj, is_active=True)
+
+        return ProductSerializer(
+            qs,
+            many=True,
+            context=self.context
+        ).data
+
